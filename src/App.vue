7eb8bb9904dd3cc3715cfaf34c4cdabbae405fae
Margus Käen <margus.kaen@gmail.com>
2026-06-23 09:49:35 +0300

    Added footer and MK Vibecode logo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <TheHeader />
-  <div class="container mt-4">
-    <RouterView />
-  </div>
+    <div class="container my-4">
+      <RouterView />
+    </div>
+  <TheFooter />
 </template>
 
 
 <script setup>
 import TheHeader from './shared/components/TheHeader/TheHeader.vue';
+import TheFooter from './shared/components/TheFooter/TheFooter.vue';
 </script>
 
 
---
+++ src/shared/components/TheFooter/TheFooter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="fixed-bottom">
+        <div class="container"> 
+            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <div class="col-md-4 d-flex align-items-center">
+                    <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1" aria-label="Bootstrap">
+                        <img src="../../../assets/mk-vibecode-logo.svg" alt="VibeCode Logo" width="60" height="48">
+                    </a>
+                    <span class="mb-3 mb-md-0 text-body-secondary">© 2026 Company, Inc</span>
+                </div>
+                <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+                    <li class="ms-3"><a class="text-body-secondary" href="#" aria-label="Instagram"><svg class="bi" width="24" height="24" aria-hidden="true"><use xlink:href="#instagram"></use></svg></a></li>
+                    <li class="ms-3"><a class="text-body-secondary" href="#" aria-label="Facebook"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+                </ul>
+            </footer>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+
+</script>
+
+
+<style scoped>
+
+</style>
